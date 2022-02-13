a46a1a44cbd0f8285d39707417cb41a46637933f
mrdrmitten <rayxmagner1@gmail.com>
2022-02-13 09:47:04 -0600

    CORS

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,14 @@ const passport = require('passport');
 const authenticate = require('../authenticate');
 const user = require('../models/user');
 
+const cors = require('./cors');
+
 const router = express.Router();
 
 /* GET users listing. */
 router.route('/')
-.get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   User.find()
   .then(users => {
     res.statusCode = 200;
@@ -18,7 +21,7 @@ router.route('/')
   .catch(err => next(err));
 })
 
-router.post('/signup', (req, res) => {
+router.post('/signup', cors.corsWithOptions, (req, res) => {
   User.register(
     new User({username: req.body.username}),
     req.body.password,
@@ -52,14 +55,14 @@ router.post('/signup', (req, res) => {
   );  
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
   const token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', (req, res, next) => {
+router.get('/logout', cors.corsWithOptions, (req, res, next) => {
     if (req.session) {
         req.session.destroy();
         res.clearCookie('session-id');
